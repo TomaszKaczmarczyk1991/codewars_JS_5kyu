842483d93817df9d27cfe117efde572c83424f32
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-03-07 16:58:48 +0000

    handle plateaus

--- pickPeaks.js
+++ pickPeaks.js
@@ -3,12 +3,16 @@ function pickPeaks(arr){
     const maxPos = [];
 
     for(let i = 1; i < arr.length-1; i++) {
-      if((arr[i] > arr[i-1]) && arr[i] > arr[i+1]) {
-        maxArr.push(arr[i]);
-        maxPos.push(i);
+      if((arr[i] > arr[i-1]) {
+        let j = i;
+        while(arr[j] === arr[j+1])
+          j++;
+          
+          if(arr[j] > arr[j+1]) {
+            maxArr.push(arr[i]);
+            maxPos.push(i);
+          }
       }
     }
     return {pos: maxPos, peaks: maxArr}
 }
-
-// test commit on GitHub Codespace
