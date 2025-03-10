5a258fa6f0ab24df0223f740db115bcaa24d34ec
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-03-10 15:27:53 +0100

    add solution

--- flattenTwoLevels.js
+++ flattenTwoLevels.js
@@ -1,10 +1,19 @@
 function flattenTwoLevels(array) {
-
-
+    const finalArr = [];
     
-    return array;
+    array.forEach((item, index) => {
+        if (Array.isArray(item)) {
+            finalArr.push(item.flat(Infinity));
+        }
+        else {
+          finalArr.push(item);
+        }
+    }) 
+    return finalArr;
   }
 
+
+console.log(flattenTwoLevels([1, [2, 3], [4, 5, [6, 7, 8], 9, 10, [11, [12, [13, 14], 15], 16], 17], 18]))
 
 // array = [1, [2, 3], [4, 5, [6, 7, 8], 9, 10, [11, [12, [13, 14], 15], 16], 17], 18];
 // return [1,[2,3],[4,5,6,7,8,9,10,11,12,13,14,15,16,17], 18]
